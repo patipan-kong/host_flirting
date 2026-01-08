1412f979ece97f2d8920da4269f0fd85bafe5ffe
Patipan Kongsirikul <patipan0@hotmail.com>
2026-01-08 17:05:00 +0700

    add allowedHosts

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,8 @@ export default defineConfig({
     host: '0.0.0.0',
     port: process.env.PORT || 5173, // Fallback for local development,
     allowedHosts: [
-      'host-flirting.onrender.com'
+      'host-flirting.onrender.com',
+      'ai-host-ek4c.onrender.com'
     ]
   }
 })
